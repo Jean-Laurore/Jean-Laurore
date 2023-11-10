e000784b9860f0be212ab3eabb3677f67075e706
jelaurore <jeanlauroree@gmail.com>
2023-11-10 10:38:10 -0500

    fixing bug

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,19 +1,19 @@
-// Get the dynamic PNG element
-const dynamicPng = document.querySelector('.dynamic-png');
+// // Get the dynamic PNG element
+// const dynamicPng = document.querySelector('.dynamic-png');
 
-// Array of image sources with different color variations
-const imageSources = [
-  '/src/images/image-white.png',
-  '/src/images/image-black.png',
-  '/src/images/image-red.png',
-];
+// // Array of image sources with different color variations
+// const imageSources = [
+//   '/src/images/image-white.png',
+//   '/src/images/image-black.png',
+//   '/src/images/image-red.png',
+// ];
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function changeImage() {
-  dynamicPng.src = imageSources[currentIndex];
-  currentIndex = (currentIndex + 1) % imageSources.length;
-}
+// function changeImage() {
+//   dynamicPng.src = imageSources[currentIndex];
+//   currentIndex = (currentIndex + 1) % imageSources.length;
+// }
 
-// Change the image every 2 seconds
-setInterval(changeImage, 2000);
+// // Change the image every 2 seconds
+// setInterval(changeImage, 2000);
